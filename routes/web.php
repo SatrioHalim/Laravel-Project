4242f73417d14c78f1076f5581c753486a867f1a
SatrioHalim <satriohalim21@gmail.com>
2025-03-01 18:12:48 +0700

    Nambahin blog Posts, View data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,14 +14,67 @@ Route::get('/about', function () {
         'title' => 'About Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog',[
-        'writer1' => 'Satrio Halim Abdurrahman',
-        'date1' => '1 Januari 2012',
-        'writer2' => 'Charles Leclerc',
-        'date2' => '17 Agustus 2023',
-        'title' => 'Blog Page'
+Route::get('/posts', function () {
+    return view('posts',[
+        'title' => 'Blog Page',
+        'posts' => [
+            [
+                'id' => '1',
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Satrio Halim Abdurrahman',
+                'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Debitis corrupti dicta libero enim reprehenderit autem vitae distinctio 
+                cupiditate aliquam, voluptates temporibus placeat consequatur quisquam 
+                asperiores, expedita ipsa iusto maiores! Provident?'
+            ],
+            [
+                'id' => '2',
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Satrio Halim Abdurrahman',
+                'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Debitis corrupti dicta libero enim reprehenderit autem vitae distinctio 
+                cupiditate aliquam, voluptates temporibus placeat consequatur quisquam 
+                asperiores, expedita ipsa iusto maiores! Provident?'
+            ]
+        ]
     ]);
+});
+
+Route::get('/posts/{slug}', function($slug) {
+    $posts = [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Satrio Halim Abdurrahman',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Debitis corrupti dicta libero enim reprehenderit autem vitae distinctio 
+            cupiditate aliquam, voluptates temporibus placeat consequatur quisquam 
+            asperiores, expedita ipsa iusto maiores! Provident?'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Satrio Halim Abdurrahman',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Debitis corrupti dicta libero enim reprehenderit autem vitae distinctio 
+            cupiditate aliquam, voluptates temporibus placeat consequatur quisquam 
+            asperiores, expedita ipsa iusto maiores! Provident?'
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', [
+        'title' => 'Single Post',
+        'post' => $post
+    ]);
+
 });
 
 Route::get('/contact', function () {
