18b86c33120d6c7985c04d83eea41d8a2a978397
SatrioHalim <satriohalim21@gmail.com>
2025-03-08 12:34:06 +0700

    Post Category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,7 +12,10 @@ class Post extends Model
     protected $fillable = ['title','author','slug','body'];
 
     public function author(): BelongsTo {
-        
         return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class);
     }
 }
